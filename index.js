f5d0e689eb32d58e552f415116a6508f0771d807
vicanso <vicansocanbico@gmail.com>
2016-01-18 22:50:44 +0800

    commit

---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict';
+const Client = require('./lib/client');
+
+
+const client = new Client({
+	database: 'mydb'
+});
+
+client.query('cpu_load_short')
+	.tag('region', 'us-west')
+	.field('value')
+	.end().then(data => {
+		console.dir(data);
+	}).catch(err => {
+		console.dir(err);
+	});
+
+// client.write('cpu_load_short')
+// 	.tag({
+// 		host: 'server01',
+// 		region: 'us-west'
+// 	})
+// 	.field('value', Math.floor(Math.random() * 100) / 100)
+// 	.end().then(data => {
+// 		console.dir(data);
+// 	}).catch(err => {
+// 		console.dir(err);
+// 	});
+
+
