16056bd716c6b49c9d504dae31cad3a65db0da01
vicanso <vicansocanbico@gmail.com>
2016-02-17 22:25:09 +0800

    commit

--- test/client.js
+++ test/client.js
@@ -5,6 +5,7 @@ const _ = require('lodash');
 
 describe('simple-influx', () => {
 	const client = new Client({
+		host: 'white',
 		database: 'mydb'
 	});
 	const series = 'http';
@@ -14,12 +15,13 @@ describe('simple-influx', () => {
 	it('create database success', done => {
 		client.createDatabase()
 			.then(data => {
-				// undefined
-				console.dir(data);
+				// data -> undefined
+				assert.equal(data, undefined);
 				done();
 			})
 			.catch(done);
 	});
+
 
 	it('write point success', done => {
 		const tags = {
@@ -38,34 +40,354 @@ describe('simple-influx', () => {
 			.value('use', 30)
 			.end()
 			.then(data => {
-				// undefined
-				console.dir(data);
+				// data -> undefined
+				assert.equal(data, undefined);
 				done();
 			})
 			.catch(done);
-
 	});
+
+
+	it('write points with callback success', done => {
+		const tags = {
+			status: '50x',
+			size: '1K'
+		};
+		const values = {
+			code: 503,
+			bytes: 1010,
+			value: 1
+		};
+		client.write(series)
+			.tag(tags)
+			.tag('uuid', ++uuid)
+			.value(values)
+			.value('use', 30)
+			.end((err, data) => {
+				if (err) {
+					return done(err);
+				}
+				done();
+			});
+	});
+
 
 	it('get points success', done => {
 		client.query(series)
 			.tag('uuid', uuid)
 			.end()
 			.then(data => {
-				// {series: []}
-				console.dir(JSON.stringify(data));
+				// data -> {"series":[{"name":"http","columns":["time","bytes","code","size","status","use","uuid","value"],"values":[["2016-02-17T06:11:41.644Z",1010,400,"1K","40x",30,"1",1]]}]}
+				const keys = ["time", "bytes", "code", "size", "status", "use", "uuid", "value"];
+				assert.equal(data.series[0].name, 'http');
+				assert.equal(data.series[0].columns.join(), keys.join());
+				assert.equal(data.series[0].values[0].length, keys.length);
 				done();
 			}).catch(done);
+	});
+
+
+	it('get points with callback success', done => {
+		client.query(series)
+			.tag('uuid', uuid)
+			.end((err, data) => {
+				if (err) {
+					return done(err);
+				}
+				// data -> {"series":[{"name":"http","columns":["time","bytes","code","size","status","use","uuid","value"],"values":[["2016-02-17T06:11:41.644Z",1010,400,"1K","40x",30,"1",1]]}]}
+				const keys = ["time", "bytes", "code", "size", "status", "use", "uuid", "value"];
+				assert.equal(data.series[0].name, 'http');
+				assert.equal(data.series[0].columns.join(), keys.join());
+				assert.equal(data.series[0].values[0].length, keys.length);
+				done();
+			});
+	});
+
+
+	it('get points with multi tag(object) success', done => {
+		client.query(series)
+			.tag({
+				status: '40x',
+				size: '1K'
+			})
+			.end()
+			.then(data => {
+				// data -> {"series":[{"name":"http","columns":["time","bytes","code","size","status","use","uuid","value"],"values":[["2016-02-17T06:11:41.644Z",1010,400,"1K","40x",30,"1",1]]}]}
+				const keys = ["time", "bytes", "code", "size", "status", "use", "uuid", "value"];
+				assert.equal(data.series[0].name, 'http');
+				assert.equal(data.series[0].columns.join(), keys.join());
+				assert.equal(data.series[0].values[0].length, keys.length);
+				done();
+			}).catch(done);
+	});
+
+
+	it('get points with where(string) success', done => {
+		client.query(series)
+			.where("status='40x'")
+			.end()
+			.then(data => {
+				// data -> {"series":[{"name":"http","columns":["time","bytes","code","size","status","use","uuid","value"],"values":[["2016-02-17T06:11:41.644Z",1010,400,"1K","40x",30,"1",1]]}]}
+				const keys = ["time", "bytes", "code", "size", "status", "use", "uuid", "value"];
+				assert.equal(data.series[0].name, 'http');
+				assert.equal(data.series[0].columns.join(), keys.join());
+				assert.equal(data.series[0].values[0].length, keys.length);
+				done();
+			}).catch(done);
+	});
+
+
+	it('get points white regular expression success', done => {
+		client.query(series)
+			.where('status =~ /50./')
+			.end()
+			.then(data => {
+				// data -> {"series":[{"name":"http","columns":["time","bytes","code","size","status","use","uuid","value"],"values":[["2016-02-17T07:35:00.951Z",1010,503,"1K","50x",30,"2",1]]}]}
+				const keys = ["time", "bytes", "code", "size", "status", "use", "uuid", "value"];
+				assert.equal(data.series[0].name, 'http');
+				assert.equal(data.series[0].columns.join(), keys.join());
+				assert.equal(data.series[0].values[0].length, keys.length);
+				done();
+			}).catch(done);
+	});
+
+
+	it('get points by time success', done => {
+		let count = 0;
+		const now = Date.now();
+		const finish = () => {
+			count++;
+			if (count === 2) {
+				done();
+			}
+		};
+
+		client.query(series)
+			.where("time > now() - 2s")
+			.end()
+			.then(data => {
+				// data -> {"series":[{"name":"http","columns":["time","bytes","code","size","status","use","uuid","value"],"values":[["2016-02-17T07:51:45.307Z",1010,400,"1K","40x",30,"1",1],["2016-02-17T07:51:45.32Z",1010,503,"1K","50x",30,"2",1]]}]}
+				_.forEach(data.series, tmp => {
+					_.forEach(tmp.values, arr => {
+						assert(now - (new Date(arr[0])).getTime() < 2000);
+					});
+				});
+				finish();
+			}).catch(done);
+
+		client.query(series)
+			.where('time > now()')
+			.end()
+			.then(data => {
+				// data -> {}
+				assert(_.isEmpty(data));
+				finish();
+			}).catch(done);
+	});
+
+
+	it('write multi points success', done => {
+		const randomTags = () => {
+			return {
+				status: _.sample(['20x', '30x', '40x', '50x']),
+				size: _.sample(['1K', '10K', '50K', '100K', '300K'])
+			};
+		};
+		const randomValues = () => {
+			return {
+				code: _.random(200, 510),
+				bytes: _.random(10, 400 * 1024),
+				use: _.random(1, 1000),
+				value: _.random(1, 2)
+			};
+		};
+		const arr = [];
+		for (let i = 2; i >= 0; i--) {
+			arr.push(
+				client.write(series)
+				.tag(randomTags())
+				.value(randomValues())
+				.end()
+			);
+		}
+
+		Promise.all(arr).then(data => {
+			// data -> [null, null, null]
+			done();
+		}).catch(done);
+
+	});
+
+
+	it('get points group by multi tags success', done => {
+		client.query(series)
+			.group('status')
+			.group('size')
+			.end()
+			.then(data => {
+				// data -> {"series":[{"name":"http","tags":{"size":"100K","status":"50x"},"columns":["time","bytes","code","use","uuid","value"],"values":[["2016-02-17T08:00:49.068Z",88879,381,35,null,1]]},{"name":"http","tags":{"size":"1K","status":"30x"},"columns":["time","bytes","code","use","uuid","value"],"values":[["2016-02-17T08:00:49.068Z",80127,218,729,null,2]]},{"name":"http","tags":{"size":"1K","status":"40x"},"columns":["time","bytes","code","use","uuid","value"],"values":[["2016-02-17T08:00:48.985Z",1010,400,30,"1",1]]},{"name":"http","tags":{"size":"1K","status":"50x"},"columns":["time","bytes","code","use","uuid","value"],"values":[["2016-02-17T08:00:49Z",1010,503,30,"2",1]]},{"name":"http","tags":{"size":"50K","status":"30x"},"columns":["time","bytes","code","use","uuid","value"],"values":[["2016-02-17T08:00:49.068Z",232600,413,630,null,1]]}]}
+				assert(data.series.length);
+				const tags = _.map(data.series, item => {
+					return `${item.tags.size}-${item.tags.status}`;
+				});
+				assert.equal(tags.join(), _.union(tags).join());
+				done();
+			}).catch(done);
+	});
+
+
+	it('limit points success', done => {
+		client.query(series)
+			.group('*')
+			.limit(1)
+			.end()
+			.then(data => {
+				// data -> {"series":[{"name":"http","tags":{"size":"10K","status":"40x","uuid":""},"columns":["time","bytes","code","use","value"],"values":[["2016-02-17T08:22:14.603Z",380886,453,67,1]]},{"name":"http","tags":{"size":"1K","status":"30x","uuid":""},"columns":["time","bytes","code","use","value"],"values":[["2016-02-17T08:22:14.603Z",77190,403,588,2]]},{"name":"http","tags":{"size":"1K","status":"40x","uuid":"1"},"columns":["time","bytes","code","use","value"],"values":[["2016-02-17T08:22:14.514Z",1010,400,30,1]]},{"name":"http","tags":{"size":"1K","status":"50x","uuid":"2"},"columns":["time","bytes","code","use","value"],"values":[["2016-02-17T08:22:14.529Z",1010,503,30,1]]},{"name":"http","tags":{"size":"300K","status":"30x","uuid":""},"columns":["time","bytes","code","use","value"],"values":[["2016-02-17T08:22:14.602Z",308696,286,54,1]]}]}
+				assert(data.series.length);
+				_.forEach(data.series, tmp => {
+					assert.equal(tmp.values.length, 1);
+				});
+				done();
+			}).catch(done);
+	});
+
+
+	it('slimit points success', done => {
+		client.query(series)
+			.group('*')
+			.slimit(1)
+			.end()
+			.then(data => {
+				assert(data.series.length, 1);
+				done();
+			}).catch(done);
+	});
+
+
+	it('order points desc success', done => {
+		client.query(series)
+			.desc()
+			.end()
+			.then(data => {
+				let time;
+				_.forEach(data.series[0].values, tmp => {
+					if (time) {
+						assert(time >= tmp[0]);
+					}
+					time = tmp[0]
+				});
+				done();
+			}).catch(done);
+	});
+
+
+	it('order points asc success', done => {
+		client.query(series)
+			.asc()
+			.end()
+			.then(data => {
+				let time;
+				_.forEach(data.series[0].values, tmp => {
+					if (time) {
+						assert(time <= tmp[0]);
+					}
+					time = tmp[0]
+				});
+				done();
+			}).catch(done);
+	});
+
+
+	it('offset points success', done => {
+		let prevData;
+		client.query(series)
+			.limit(2)
+			.end()
+			.then(data => {
+				prevData = data;
+				return client.query(series)
+					.offset(1)
+					.end();
+			})
+			.then(data => {
+				assert.equal(prevData.series[0].values[1].join(','), data.series[0].values[0].join(','));
+				done();
+			}).catch(done);
+	});
+
+
+	it('mean result success', done => {
+		client.query(series)
+			.mean('use')
+			.end()
+			.then(data => {
+				// data -> {"series":[{"name":"http","columns":["time","mean"],"values":[["1970-01-01T00:00:00Z",61.8]]}]}
+				assert.equal(data.series[0].columns.join(','), 'time,mean');
+				assert.equal(data.series[0].values[0].length, 2);
+				done();
+			}).catch(done);
+	});
+
+
+	it('sum result success', done => {
+		client.query(series)
+			.sum('use')
+			.end()
+			.then(data => {
+				// data -> {"series":[{"name":"http","columns":["time","sum"],"values":[["1970-01-01T00:00:00Z",1601]]}]}
+				assert.equal(data.series[0].columns.join(','), 'time,sum');
+				assert.equal(data.series[0].values[0].length, 2);
+				done();
+			}).catch(done);
+	});
+
+
+	it('count result success', done => {
+		client.query(series)
+			.count('use')
+			.end()
+			.then(data => {
+				// data -> {"series":[{"name":"http","columns":["time","count"],"values":[["1970-01-01T00:00:00Z",5]]}]}
+				assert.equal(data.series[0].columns.join(','), 'time,count');
+				assert.equal(data.series[0].values[0].length, 2);
+				done();
+			}).catch(done);
+	});
+
+
+	it('get measurements success', done => {
+		client.getMeasurements().then(data => {
+			// data -> {"series":[{"name":"measurements","columns":["name"],"values":[["http"]]}]}
+			assert.equal(data.series[0].name, 'measurements');
+			assert.equal(data.series[0].columns[0], 'name');
+			assert.equal(data.series[0].values[0][0], 'http');
+			done();
+		}).catch(done);
+	});
+
+
+	it('drop measurement success', done => {
+		client.dropMeasurement('http').then(data => {
+			// data -> {}
+			assert(_.isEmpty(data));
+			return client.getMeasurements().then(data => {
+				// data -> {}
+				assert(_.isEmpty(data));
+				done();
+			});
+		}).catch(done);
 	});
 
 
 	it('drop database success', done => {
 		client.dropDatabase()
 			.then(data => {
-				console.dir(data);
+				// data -> undefined
+				assert.equal(data, undefined);
 				done();
 			})
 			.catch(done);
-	})
+	});
 
 	// it('get measurements success', done => {
 	// 	client.getMeasurements()
@@ -86,23 +408,23 @@ describe('simple-influx', () => {
 	// 	});
 
 	// it('write points success', done => {
-		// const tags = {
-		// 	status: '40x',
-		// 	size: '1K'
-		// };
-		// const values = {
-		// 	code: 400,
-		// 	bytes: 1010,
-		// 	value: 1
-		// };
-		// client.write(series)
-		// 	.tag(tags)
-		// 	.tag('uuid', ++uuid)
-		// 	.value(values)
-		// 	.value('use', 30)
-		// 	.end()
-		// 	.then(done)
-		// 	.catch(done);
+	// const tags = {
+	// 	status: '40x',
+	// 	size: '1K'
+	// };
+	// const values = {
+	// 	code: 400,
+	// 	bytes: 1010,
+	// 	value: 1
+	// };
+	// client.write(series)
+	// 	.tag(tags)
+	// 	.tag('uuid', ++uuid)
+	// 	.value(values)
+	// 	.value('use', 30)
+	// 	.end()
+	// 	.then(done)
+	// 	.catch(done);
 	// });
 
 
@@ -137,26 +459,26 @@ describe('simple-influx', () => {
 
 
 	// it('write points with callback success', done => {
-	// 	const tags = {
-	// 		status: '50x',
-	// 		size: '1K'
-	// 	};
-	// 	const values = {
-	// 		code: 503,
-	// 		bytes: 1010,
-	// 		value: 1
-	// 	};
-	// 	client.write(series)
-	// 		.tag(tags)
-	// 		.tag('uuid', ++uuid)
-	// 		.value(values)
-	// 		.value('use', 30)
-	// 		.end(err => {
-	// 			if (err) {
-	// 				return done(err);
-	// 			}
-	// 			done();
-	// 		});
+	// const tags = {
+	// 	status: '50x',
+	// 	size: '1K'
+	// };
+	// const values = {
+	// 	code: 503,
+	// 	bytes: 1010,
+	// 	value: 1
+	// };
+	// client.write(series)
+	// 	.tag(tags)
+	// 	.tag('uuid', ++uuid)
+	// 	.value(values)
+	// 	.value('use', 30)
+	// 	.end(err => {
+	// 		if (err) {
+	// 			return done(err);
+	// 		}
+	// 		done();
+	// 	});
 	// });
 
 
@@ -217,11 +539,11 @@ describe('simple-influx', () => {
 	// 			if (err) {
 	// 				return done(err);
 	// 			}
-	// 			_.forEach(data.series, tmp => {
-	// 				_.forEach(tmp.values, arr => {
-	// 					assert(now - (new Date(arr[0])).getTime() < 2000);
-	// 				});
-	// 			});
+	// _.forEach(data.series, tmp => {
+	// 	_.forEach(tmp.values, arr => {
+	// 		assert(now - (new Date(arr[0])).getTime() < 2000);
+	// 	});
+	// });
 	// 			finish();
 	// 		});
 	// 	client.query(series)
@@ -238,21 +560,21 @@ describe('simple-influx', () => {
 
 	// it('write multi points success', done => {
 
-	// 	function randomTags() {
-	// 		return {
-	// 			status: _.sample(['20x', '30x', '40x', '50x']),
-	// 			size: _.sample(['1K', '10K', '50K', '100K', '300K'])
-	// 		};
-	// 	}
+	// function randomTags() {
+	// 	return {
+	// 		status: _.sample(['20x', '30x', '40x', '50x']),
+	// 		size: _.sample(['1K', '10K', '50K', '100K', '300K'])
+	// 	};
+	// }
 
-	// 	function randomValues() {
-	// 		return {
-	// 			code: _.random(200, 510),
-	// 			bytes: _.random(10, 400 * 1024),
-	// 			use: _.random(1, 1000),
-	// 			value: _.random(1, 2)
-	// 		}
+	// function randomValues() {
+	// 	return {
+	// 		code: _.random(200, 510),
+	// 		bytes: _.random(10, 400 * 1024),
+	// 		use: _.random(1, 1000),
+	// 		value: _.random(1, 2)
 	// 	}
+	// }
 	// 	const arr = [];
 	// 	for (let i = 5; i >= 0; i--) {
 	// 		arr.push(
