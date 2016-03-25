ba6f502d927f68a9ae3210533f0ac00847c3f437
vicanso <vicansocanbico@gmail.com>
2016-03-25 23:01:59 +0800

    use eslint-config-airbnb

--- examples/koa.js
+++ examples/koa.js
@@ -5,34 +5,34 @@ const Influx = require('..');
 const client = new Influx('http://localhost:8086/my-app');
 const _ = require('lodash');
 
-app.use(function *(next) {
-	const ctx = this;
-	const start = Date.now();
-	yield *next;
-	const statusCode = ctx.status;
-	const use = Date.now() - start;
-	const tags = {
-		status: _.sortedIndex([99, 199, 299, 399, 499, 599], statusCode),
-		spdy: _.sortedIndex([100, 300, 1000, 3000], use)
-	};
-	const fields = {
-		use: use,
-		code: statusCode
-	};
-	client.writePoint('http', fields, tags).then(() => {
-		console.info('write point to http measurement success');
-	}).catch(err => {
-		console.error(err);
-	});
+app.use(function*(next) {
+  const ctx = this;
+  const start = Date.now();
+  yield * next;
+  const statusCode = ctx.status;
+  const use = Date.now() - start;
+  const tags = {
+    status: _.sortedIndex([99, 199, 299, 399, 499, 599], statusCode),
+    spdy: _.sortedIndex([100, 300, 1000, 3000], use)
+  };
+  const fields = {
+    use: use,
+    code: statusCode
+  };
+  client.writePoint('http', fields, tags).then(() => {
+    console.info('write point to http measurement success');
+  }).catch(err => {
+    console.error(err);
+  });
 });
 
-app.use(function *(){
-	yield new Promise((resolve, reject) => {
-		setTimeout(() => {
-			this.body = 'Hello World';
-			resolve();
-		}, 1000);
-	});
+app.use(function*() {
+  yield new Promise((resolve, reject) => {
+    setTimeout(() => {
+      this.body = 'Hello World';
+      resolve();
+    }, 1000);
+  });
 });
 
 app.listen(3000);
